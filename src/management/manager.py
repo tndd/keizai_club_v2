746b2fbfcc1c37fed0171e8a0c3f3dbfbeb9916d
tau <ttru702@gmail.com>
2022-08-14 20:23:08 +0900

    update content_category

---
+++ src/management/manager.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass
+from pathlib import Path
+from typing import List
+
+import yaml
+
+
+@dataclass
+class Manager:
+    pwd: str = Path(__file__).resolve().parent
+    file_category: str = 'content_category.yml'
+    file_location: str = 'content_location.yml'
+    file_page_url: str = 'content_page_url.yml'
+
+    def load_content_category(self) -> dict:
+        with open(f'{self.pwd}/{self.file_category}', 'r') as f:
+            d = yaml.safe_load(f)
+        return d
+
+    def load_content_location(self) -> dict:
+        with open(f'{self.pwd}/{self.file_location}', 'r') as f:
+            d = yaml.safe_load(f)
+        return d
+
+    def load_content_page_url(self) -> dict:
+        with open(f'{self.pwd}/{self.file_page_url}', 'r') as f:
+            d = yaml.safe_load(f)
+        return d
+
+    def get_categories_url(self) -> List[str]:
+        data_category = self.load_content_category()
+        return [url for url in data_category.keys()]
+
+
+def main() -> None:
+    m = Manager()
+    print(m.get_categories_url())
+
+
+if __name__ == '__main__':
+    main()
