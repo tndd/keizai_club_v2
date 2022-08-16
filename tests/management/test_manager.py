9255b17c9d668079c9a01914add5250d174dcca9
tau <ttru702@gmail.com>
2022-08-16 16:51:12 +0900

    update test_manager

--- tests/management/test_manager.py
+++ tests/management/test_manager.py
@@ -9,9 +9,14 @@ def test_init_db() -> None:
     m = get_manager_for_test()
     m.init_db()
 
+def test_select_category() -> None:
+    m = get_manager_for_test()
+    l = m.select_category()
+    print(l)
+
 
 def main() -> None:
-    test_init_db()
+    test_select_category()
 
 
 if __name__ == '__main__':
