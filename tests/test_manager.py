b17a048e01d11c258f2daaa2bca6f234b96fe3e1
tau <ttru702@gmail.com>
2022-08-17 17:46:47 +0900

    complete implement test_init_db

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -13,11 +13,19 @@ def test_init_db() -> None:
     m.init_db()
     # validate table name
     query_get_tables = "SELECT name FROM sqlite_master WHERE type='table' ORDER BY NAME;"
-    expected_table_names = [('category',), ('location',), ('page_url',)]
-    table_names = m.cur.execute(query_get_tables).fetchall()
-    assert expected_table_names == table_names
-    # print(m.cur.fetchall())
-    # print(m.cur.execute("PRAGMA table_info('category')").fetchall())
+    expd_tbl_names = ['category', 'location', 'page_url']
+    table_names = [n[0] for n in m.cur.execute(query_get_tables).fetchall()]
+    assert expd_tbl_names == table_names
+    # validate table info
+    expd_tbl_info = {
+        "category": [(0, 'url', 'TEXT', 1, None, 1), (1, 'name', 'TEXT', 1, None, 0), (2, 'name_jp', 'TEXT', 1, None, 0)],
+        "location": [(0, 'content_location', 'TEXT', 1, None, 1), (1, 'page_url', 'TEXT', 1, None, 0), (2, 'media_type', 'TEXT', 1, None, 0), (3, 'file_name', 'TEXT', 1, None, 0), (4, 'dest', 'TEXT', 1, None, 0), (5, 'content_text', 'TEXT', 0, None, 0)],
+        "page_url": [(0, 'timestamp', 'TEXT', 1, None, 1), (1, 'page_url', 'TEXT', 1, None, 2), (2, 'page_title', 'TEXT', 1, None, 0), (3, 'page_date', 'TEXT', 1, None, 0), (4, 'category', 'TEXT', 1, None, 0), (5, 'parent_url', 'TEXT', 1, None, 0)]
+    }
+    for name in table_names:
+        query_table_info = f"PRAGMA table_info('{name}');"
+        print(m.cur.execute(query_table_info).fetchall())
+        assert expd_tbl_info[name] == m.cur.execute(query_table_info).fetchall()
 
 
 # def test_select_category() -> None:
