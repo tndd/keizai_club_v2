f2f6f9b0916410b236425852b259200b80e9707f
tau <ttru702@gmail.com>
2022-08-17 17:54:17 +0900

    add test_select_category

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -28,15 +28,19 @@ def test_init_db() -> None:
         assert expd_tbl_info[name] == m.cur.execute(query_table_info).fetchall()
 
 
-# def test_select_category() -> None:
-#     m = get_manager_for_test()
-#     l = m.select_category()
-#     print(l)
+def test_select_category() -> None:
+    m = get_manager_for_test()
+    expd_rows_category = [
+        ('https://keizaiclub.com/category/%e8%bf%91%e6%9c%aa%e6%9d%a5%e4%ba%88%e6%b8%ac', 'near_future', '近未来予測'),
+        ('https://keizaiclub.com/category/zatsudan', 'wrestling_chat', 'プロレス雑談'),
+        ('https://keizaiclub.com/category/%e3%83%a9%e3%82%a4%e3%83%96qa', 'live_qa', 'ライブQ&A'),
+        ('https://keizaiclub.com/category/%e3%83%9d%e3%83%83%e3%83%89%e3%82%ad%e3%83%a3%e3%82%b9%e3%83%86%e3%82%a3%e3%83%b3%e3%82%b0', 'podcast', '世界経済Q&A')
+    ]
+    assert expd_rows_category == m.select_category()
 
 
 def main() -> None:
-    pass
-    test_init_db()
+    test_select_category()
 
 
 if __name__ == '__main__':
