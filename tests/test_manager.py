882a4a960cc74db59d32b5537742e22d7faa8ea9
tau <ttru702@gmail.com>
2022-08-17 17:33:33 +0900

    test pytest

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -12,17 +12,22 @@ def test_init_db() -> None:
     # test method
     m.init_db()
     # validate table name
-    m.cur.execute("SELECT name FROM sqlite_master WHERE type='table' ORDER BY NAME;")
-    print(m.cur.fetchall())
+    query_get_tables = "SELECT name FROM sqlite_master WHERE type='table' ORDER BY NAME;"
+    expected_table_names = [('category',), ('location',), ('page_url',)]
+    table_names = m.cur.execute(query_get_tables).fetchall()
+    assert expected_table_names == table_names
+    # print(m.cur.fetchall())
+    # print(m.cur.execute("PRAGMA table_info('category')").fetchall())
 
 
-def test_select_category() -> None:
-    m = get_manager_for_test()
-    l = m.select_category()
-    print(l)
+# def test_select_category() -> None:
+#     m = get_manager_for_test()
+#     l = m.select_category()
+#     print(l)
 
 
 def main() -> None:
+    pass
     test_init_db()
 
 
